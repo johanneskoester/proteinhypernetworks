81c265ba51b787e369f0581ab46805a79ad68a40
johannes.koester@gmail.com <johannes.koester@gmail.com@130ca62d-aca6-da52-f75f-5f55d868bd0f>
2011-06-01 09:21:21 +0000

    Initial import of sidebar library.

---
+++ Sidebar/src/sidebar/SidebarTreeCellRenderer.java
@@ -0,0 +1,29 @@
+/* Copyright (c) 2010, Johannes Köster <johannes.koester@tu-dortmund.de>
+ * All rights reserved.
+ *
+ * This software is open-source under the BSD license; see "license.txt"
+ * for a description.
+ */
+
+package sidebar;
+
+import java.awt.Color;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+/**
+ *
+ * @author Johannes Köster <johannes.koester@tu-dortmund.de>
+ */
+public class SidebarTreeCellRenderer extends DefaultTreeCellRenderer {
+
+  @Override
+  public Color getBackgroundNonSelectionColor() {
+    return null;
+  }
+
+  @Override
+  public Color getBackground() {
+    return null;
+  }
+
+}
