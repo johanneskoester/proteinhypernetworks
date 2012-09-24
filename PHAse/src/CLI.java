ebb2b30e49f3ddc9b241f8fa11d0ea91af64e9c9
johannes.koester@gmail.com <johannes.koester@gmail.com@130ca62d-aca6-da52-f75f-5f55d868bd0f>
2012-09-24 15:20:46 +0000

    Minor.

--- PHAse/src/CLI.java
+++ PHAse/src/CLI.java
@@ -1,9 +1,20 @@
+/* Copyright (c) 2010, Johannes Köster <johannes.koester@tu-dortmund.de>
+ * All rights reserved.
+ *
+ * This software is open-source under the BSD license; see "license.txt"
+ * for a description.
+ */
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 import javax.xml.stream.XMLStreamException;
+
+import logicProteinHypernetwork.analysis.complexes.Complex;
+import logicProteinHypernetwork.analysis.pis.PIS;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -12,52 +23,56 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.cli.PosixParser;
 
-
-
-
+/**
+ * 
+ * @author Johannes Köster <johannes.koester@tu-dortmund.de>
+ */
 public class CLI {
 	public static String usage = "PHAse <input> <output>";
-	
+
 	public static void main(String[] args) {
 		CommandLineParser parser = new PosixParser();
 		Options options = new Options();
 		options.addOption("h", "help", false, "Print this message.");
-		options.addOption("t", "threads", false, "Number of threads to use (1 per default).");
+		options.addOption("t", "threads", false,
+				"Number of threads to use (1 per default).");
 		options.addOption("c", "complexes", false, "Predict protein complexes.");
 		options.addOption("p", "pis", false, "Predict perturbation impact.");
 		// automatically generate the help statement
 		HelpFormatter help = new HelpFormatter();
-		
+
 		String input = null;
 		String output = null;
+		CommandLine line = null;
 		try {
-			CommandLine line = parser.parse(options, args);
-			if(line.hasOption("help")) {
+			line = parser.parse(options, args);
+			if (line.hasOption("help")) {
 				help.printHelp(usage, options);
 				return;
 			}
-			
+
 			String[] positional = line.getArgs();
 			input = positional[0];
 			output = positional[1];
 			System.out.println(input);
-			
-			Controller.getInstance().setThreads(Integer.valueOf(line.getOptionValue("threads", "1")));
 		} catch (ParseException e) {
-			help.printHelp(usage, options );
+			help.printHelp(usage, options);
 		} catch (ArrayIndexOutOfBoundsException e) {
-			help.printHelp(usage, options );
+			help.printHelp(usage, options);
 		}
-		if(input == null || output == null) {
+		if (input == null || output == null || line == null) {
 			return;
 		}
-		
+
 		File infile = new File(input);
 		BufferedWriter outstream = null;
-		if(output.equals("-")) {
+		if (output.equals("-")) {
 			outstream = new BufferedWriter(new OutputStreamWriter(System.out));
 		}
-		
+
+		Controller.getInstance().setThreads(
+				Integer.valueOf(line.getOptionValue("threads", "1")));
+
 		try {
 			Controller.getInstance().loadHypernetwork(infile);
 		} catch (FileNotFoundException e) {
@@ -67,7 +82,29 @@ public class CLI {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		System.out.println(input);
+
+		if (line.hasOption("complexes")) {
+			Controller.getInstance().predictComplexes();
+			try {
+				for (Complex c : Controller.getInstance().getComplexes()) {
+					outstream.append(c.toString());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		if (line.hasOption("pis")) {
+			Controller.getInstance().predictPIS();
+			try {
+				for(PIS pis : Controller.getInstance().getPIS()) {
+					outstream.append(pis.toString());
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
 	}
 }
--- PHAse/src/Controller.java
+++ PHAse/src/Controller.java
@@ -1,24 +1,39 @@
+/* Copyright (c) 2010, Johannes Köster <johannes.koester@tu-dortmund.de>
+ * All rights reserved.
+ *
+ * This software is open-source under the BSD license; see "license.txt"
+ * for a description.
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import javax.xml.stream.XMLStreamException;
 
 import logicProteinHypernetwork.LogicProteinHypernetwork;
+import logicProteinHypernetwork.analysis.complexes.Complex;
+import logicProteinHypernetwork.analysis.pis.PIS;
 import proteinHypernetwork.ProteinHypernetwork;
 import proteinHypernetwork.decoder.HypernetworkMLDecoder;
 
-
+/**
+*
+* @author Johannes Köster <johannes.koester@tu-dortmund.de>
+*/
 public class Controller {
 	private static Controller instance = new Controller();
-	
+
 	private int threads = 1;
 	private ProteinHypernetwork hypernetwork;
 	private LogicProteinHypernetwork logicHypernetwork;
-	
+
 	public static Controller getInstance() {
 		return instance;
 	}
-	
+
 	public int getThreads() {
 		return threads;
 	}
@@ -27,10 +42,34 @@ public class Controller {
 		this.threads = threads;
 	}
 
-	public void loadHypernetwork(File file) throws FileNotFoundException, XMLStreamException {
+	public void loadHypernetwork(File file) throws FileNotFoundException,
+			XMLStreamException {
 		HypernetworkMLDecoder decoder = new HypernetworkMLDecoder();
 		hypernetwork = new ProteinHypernetwork();
 		decoder.decode(file, hypernetwork);
 		logicHypernetwork = new LogicProteinHypernetwork(hypernetwork, threads);
+	}
+
+	public void predictComplexes() {
+		try {
+			logicHypernetwork.predictComplexes();
+			for (Complex c : logicHypernetwork.getComplexes()) {
+				Collections.sort(c);
+			}
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void predictPIS() {
+		logicHypernetwork.predictPIS();
+	}
+	
+	public Collection<Complex> getComplexes() {
+		return logicHypernetwork.getComplexes();
+	}
+	
+	public List<? extends PIS> getPIS() {
+		return logicHypernetwork.getPIS();
 	}
 }
